c579d97059ffef64b5a8dea3ac9a68056f0c4eff
Nils Asmussen <nils@script-solution.de>
2024-10-13 09:30:28 +0200

    Added tests for simple date-range functionality.

--- src/objects/item.rs
+++ src/objects/item.rs
@@ -48,12 +48,12 @@ fn is_within(
     }
 
     if let Some(ev_start) = ev_start {
-        if ical_date_to_utc(ev_start) < start {
+        if ical_date_to_utc(ev_start) > end {
             return false;
         }
     }
     if let Some(ev_end) = ev_end {
-        if ical_date_to_utc(ev_end) > end {
+        if ical_date_to_utc(ev_end) < start {
             return false;
         }
     }
@@ -110,7 +110,7 @@ impl CalItem {
                 start,
                 end,
                 ev.get_start(),
-                ev.get_end(),
+                ev.get_due(),
                 ev.property_value("RRULE"),
             ),
             _ => false,
@@ -131,5 +131,103 @@ impl CalItem {
             .iter()
             .filter(|&c| matches!(c, icalendar::CalendarComponent::Event(_)))
             .map(|e| e.as_event().unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::NaiveDate;
+    use icalendar::EventLike;
+
+    use super::*;
+
+    fn new_date(year: i32, month: u32, day: u32) -> DateTime<Utc> {
+        Utc.with_ymd_and_hms(year, month, day, 0, 0, 0).unwrap()
+    }
+
+    fn new_allday_event(date: NaiveDate, uid: &str) -> icalendar::Event {
+        icalendar::Event::new().all_day(date).uid(uid).done()
+    }
+
+    fn has_uids<'a, I: Iterator<Item = &'a CalendarComponent>>(result: I, uids: &[&str]) -> bool {
+        let result = result.collect::<Vec<_>>();
+        assert_eq!(result.len(), uids.len());
+        for uid in uids {
+            if result
+                .iter()
+                .find(|c| c.as_event().unwrap().get_uid().unwrap() == *uid)
+                .is_none()
+            {
+                return false;
+            }
+        }
+        true
+    }
+
+    #[test]
+    fn items_within_simple() {
+        let mut cal = icalendar::Calendar::new();
+        cal.push(new_allday_event(
+            NaiveDate::from_ymd_opt(2024, 10, 2).unwrap(),
+            "yes1",
+        ));
+        cal.push(new_allday_event(
+            NaiveDate::from_ymd_opt(2024, 10, 1).unwrap(),
+            "yes2",
+        ));
+        cal.push(new_allday_event(
+            NaiveDate::from_ymd_opt(2024, 10, 31).unwrap(),
+            "yes3",
+        ));
+        cal.push(new_allday_event(
+            NaiveDate::from_ymd_opt(2023, 10, 31).unwrap(),
+            "no1",
+        ));
+        cal.push(new_allday_event(
+            NaiveDate::from_ymd_opt(2024, 9, 30).unwrap(),
+            "no2",
+        ));
+
+        let item = CalItem::new(0, "".into(), cal);
+        let items = item.items_within(new_date(2024, 10, 1), new_date(2024, 10, 31));
+        assert!(has_uids(items, &["yes1", "yes2", "yes3"]));
+    }
+
+    #[test]
+    fn items_within_missing() {
+        let mut cal = icalendar::Calendar::new();
+        cal.push(new_allday_event(
+            NaiveDate::from_ymd_opt(1990, 1, 4).unwrap(),
+            "yes1",
+        ));
+        cal.push(icalendar::Event::new().uid("yes2").done());
+        cal.push(
+            icalendar::Event::new()
+                .starts(NaiveDate::from_ymd_opt(1990, 1, 5).unwrap())
+                .uid("yes3")
+                .done(),
+        );
+        cal.push(
+            icalendar::Event::new()
+                .ends(NaiveDate::from_ymd_opt(1990, 10, 1).unwrap())
+                .uid("yes4")
+                .done(),
+        );
+        cal.push(
+            icalendar::Event::new()
+                .starts(NaiveDate::from_ymd_opt(2000, 2, 1).unwrap())
+                .uid("no1")
+                .done(),
+        );
+        cal.push(
+            icalendar::Event::new()
+                .ends(NaiveDate::from_ymd_opt(1989, 12, 31).unwrap())
+                .uid("no2")
+                .done(),
+        );
+
+        let item = CalItem::new(0, "".into(), cal);
+        let items = item.items_within(new_date(1990, 1, 1), new_date(2000, 1, 31));
+        assert!(has_uids(items, &["yes1", "yes2", "yes3", "yes4"]));
     }
 }
