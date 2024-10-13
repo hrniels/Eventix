688224371b6b4f53719eb51a709c7198fb5da9f3
Nils Asmussen <nils@script-solution.de>
2024-10-13 07:14:57 +0200

    Added id to calendar source.

--- src/objects/source.rs
+++ src/objects/source.rs
@@ -5,15 +5,18 @@ use std::{
     path::PathBuf,
 };
 
-use super::CalItem;
+use super::{CalItem, Id};
 
 pub struct CalSource {
+    id: Id,
     path: PathBuf,
     items: Vec<CalItem>,
 }
 
 impl CalSource {
     pub fn new_from_dir(path: PathBuf) -> Result<Self, anyhow::Error> {
+        let id = super::generate_id();
+
         let mut items = Vec::new();
         for e in read_dir(path.as_path())? {
             let filename = e?.path();
@@ -24,10 +27,15 @@ impl CalSource {
             let cal = input.parse::<icalendar::Calendar>().map_err(|e| {
                 anyhow!("Parsing calendar in {:?} failed: {}", filename.as_path(), e)
             })?;
-            let cal = CalItem::new(filename, cal);
+            let cal = CalItem::new(id, filename, cal);
             items.push(cal);
         }
-        Ok(Self { path, items })
+
+        Ok(Self { id, path, items })
+    }
+
+    pub fn id(&self) -> Id {
+        self.id
     }
 
     pub fn items(&self) -> &[CalItem] {
