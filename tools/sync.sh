337d46589fdbd13f46522afbb147d7276c85aae2
Nils Asmussen <nils@script-solution.de>
2025-07-26 10:32:18 +0200

    tools: use the test config in sync.sh.

--- tools/sync.sh
+++ tools/sync.sh
@@ -3,12 +3,13 @@
 discover=false
 if [ "$1" = "--discover" ]; then
     discover=true
+    shift
 fi
 
 source "$HOME/.config/systemd/user/vdirsyncer.key"
 export KEEPASS_PW
 
 if $discover; then
-    yes | vdirsyncer discover
+    yes | vdirsyncer -c "$HOME/.config/vdirsyncer-test/config" discover
 fi
-vdirsyncer sync
+vdirsyncer -c "$HOME/.config/vdirsyncer-test/config" sync "$@"
