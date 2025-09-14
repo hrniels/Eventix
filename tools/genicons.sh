e870e25cf12060a34af5dacbc00e995b7025d1d0
Nils Asmussen <nils@script-solution.de>
2025-09-14 07:39:53 +0200

    eventix: removed 32x32 icon.

--- tools/genicons.sh
+++ tools/genicons.sh
@@ -3,7 +3,7 @@
 input="data/icons/scalable.svg"
 outdir="data/icons"
 
-for size in 32 48 64 128 256; do
+for size in 48 64 128 256; do
     convert -background none "$input" -resize "${size}x${size}" "$outdir/${size}x${size}.png"
 done
 convert -background none "$input" -resize "512x512" "data/static/icon.png"
