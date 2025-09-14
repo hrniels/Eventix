cf65257a1c4caf5217a65a5ba4f495ad8fe0e1b8
Nils Asmussen <nils@script-solution.de>
2025-09-14 07:29:56 +0200

    eventix: created SVG from scratch and regenerated PNGs.

--- tools/genicons.sh
+++ tools/genicons.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-input="data/static/icon.png"
+input="data/icons/scalable.svg"
 outdir="data/icons"
 
 for size in 32 48 64 128 256; do
-    convert "$input" -resize "${size}x${size}" "$outdir/${size}x${size}.png"
+    convert -background none "$input" -resize "${size}x${size}" "$outdir/${size}x${size}.png"
 done
+convert -background none "$input" -resize "512x512" "data/static/icon.png"
