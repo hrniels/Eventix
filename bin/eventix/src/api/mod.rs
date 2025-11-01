2ec3dc972486afc94b9ac09e2d5444cb9443df96
Nils Asmussen <nils@script-solution.de>
2025-11-01 07:46:00 +0100

    eventix: improved error reporting in API.

--- bin/eventix/src/api/mod.rs
+++ bin/eventix/src/api/mod.rs
@@ -5,10 +5,11 @@ pub mod help;
 pub mod items;
 pub mod togglecal;
 
+use anyhow::Chain;
 use axum::{
     Json, Router,
     body::{Body, to_bytes},
-    http::{Request, StatusCode},
+    http::{HeaderValue, Request, StatusCode},
     middleware::Next,
     response::{IntoResponse, Response},
 };
@@ -20,6 +21,21 @@ pub struct JsonError {
     inner: anyhow::Error,
 }
 
+impl JsonError {
+    fn generate_message(&self) -> String {
+        let mut msg = format!("{}", self.inner);
+
+        if let Some(cause) = self.inner.source() {
+            msg.push_str("\n\nCaused by:");
+            for (n, error) in Chain::new(cause).enumerate() {
+                msg.push_str(&format!("\n  {}: {}", n + 1, error));
+            }
+        }
+
+        msg
+    }
+}
+
 impl From<anyhow::Error> for JsonError {
     fn from(err: anyhow::Error) -> Self {
         Self { inner: err }
@@ -28,11 +44,14 @@ impl From<anyhow::Error> for JsonError {
 
 impl IntoResponse for JsonError {
     fn into_response(self) -> Response {
+        tracing::debug!("request failed: {:?}", self.inner);
+
         let body = Json(json!({
-            "error": format!("{:?}", self.inner),
+            "error": self.generate_message(),
         }));
 
-        (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
+        // use a temporary and otherwise unused error code to simply keep the body below
+        (StatusCode::CONTINUE, body).into_response()
     }
 }
 
@@ -42,25 +61,40 @@ async fn json_error_middleware(req: Request<Body>, next: Next) -> Response {
     if !res.status().is_success() {
         // extract the body bytes (consumes it)
         let status = res.status();
-        let bytes = to_bytes(res.into_body(), 1024 * 16).await;
 
-        // build a new JSON body
-        let msg = match bytes {
-            Ok(b) => match String::from_utf8(b.to_vec()) {
-                Ok(s) if !s.is_empty() => s,
-                _ => status
-                    .canonical_reason()
-                    .unwrap_or("Unknown error")
-                    .to_string(),
-            },
-            Err(_) => status
-                .canonical_reason()
-                .unwrap_or("Unknown error")
-                .to_string(),
+        let mut resp = match status {
+            StatusCode::CONTINUE => {
+                (StatusCode::INTERNAL_SERVER_ERROR, res.into_body()).into_response()
+            }
+            status => {
+                let bytes = to_bytes(res.into_body(), 1024 * 16).await;
+
+                // build a new JSON body
+                let msg = match bytes {
+                    Ok(b) => match String::from_utf8(b.to_vec()) {
+                        Ok(s) if !s.is_empty() => s,
+                        _ => status
+                            .canonical_reason()
+                            .unwrap_or("Unknown error")
+                            .to_string(),
+                    },
+                    Err(_) => status
+                        .canonical_reason()
+                        .unwrap_or("Unknown error")
+                        .to_string(),
+                };
+
+                let json_body = Json(json!({ "error": msg }));
+                (status, json_body).into_response()
+            }
         };
 
-        let json_body = Json(json!({ "error": msg }));
-        return (status, json_body).into_response();
+        resp.headers_mut().append(
+            "Content-Type",
+            HeaderValue::from_str("application/json").unwrap(),
+        );
+
+        return resp;
     } else {
         tracing::debug!("got status {}", res.status());
     }
