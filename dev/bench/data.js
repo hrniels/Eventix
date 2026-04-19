d5de32c19a9980c66f033150a0106ccfaec4bf10
github-action-benchmark <github@users.noreply.github.com>
2026-04-19 16:14:27 +0000

    add Eventix Monthly Benchmark (customSmallerIsBetter) benchmark result for 63de4b7c02441028c2e661485585a00d306b7240

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776615194618,
+  "lastUpdate": 1776615267045,
   "repoUrl": "https://github.com/hrniels/Eventix",
   "entries": {
     "Eventix List Benchmark": [
@@ -34,6 +34,43 @@ window.BENCHMARK_DATA = {
           {
             "name": "pages/list/results/content/keyword_or",
             "value": 5829052.574453387,
+            "unit": "ns"
+          }
+        ]
+      }
+    ],
+    "Eventix Monthly Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "hrniels",
+            "username": "hrniels"
+          },
+          "committer": {
+            "name": "hrniels",
+            "username": "hrniels"
+          },
+          "id": "63de4b7c02441028c2e661485585a00d306b7240",
+          "message": "Added benchmarks, also to CI",
+          "timestamp": "2026-04-17T18:20:55Z",
+          "url": "https://github.com/hrniels/Eventix/pull/24/commits/63de4b7c02441028c2e661485585a00d306b7240"
+        },
+        "date": 1776615266801,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pages/monthly/content/current_month",
+            "value": 6445441.544071093,
+            "unit": "ns"
+          },
+          {
+            "name": "pages/monthly/content/dense_month",
+            "value": 4773051.676426978,
+            "unit": "ns"
+          },
+          {
+            "name": "pages/monthly/content/explicit_month",
+            "value": 52970861.1,
             "unit": "ns"
           }
         ]
