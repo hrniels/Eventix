ecfea125937ba54b3362471c0bae6094cd93fdc5
github-action-benchmark <github@users.noreply.github.com>
2026-04-23 07:01:36 +0000

    add Eventix Parse Benchmark (customSmallerIsBetter) benchmark result for 66c9904f0e14a49bf1590767e4d1fcc3273a5e37

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776927663081,
+  "lastUpdate": 1776927696785,
   "repoUrl": "https://github.com/hrniels/Eventix",
   "entries": {
     "Eventix List Benchmark": [
@@ -2253,6 +2253,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "ics/parse_directory/generated_calendar_dir",
             "value": 43454419.5,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nils@script-solution.de",
+            "name": "Nils Asmussen",
+            "username": "hrniels"
+          },
+          "committer": {
+            "email": "nils@script-solution.de",
+            "name": "Nils Asmussen",
+            "username": "hrniels"
+          },
+          "distinct": true,
+          "id": "66c9904f0e14a49bf1590767e4d1fcc3273a5e37",
+          "message": "eventix: fixed modal title for errorDialog.\n\nCo-authored-by: GitHub Copilot <copilot@github.com>",
+          "timestamp": "2026-04-23T08:55:52+02:00",
+          "tree_id": "4db611c6e81f34d5eaf9ae2e9c8feb8387c41769",
+          "url": "https://github.com/hrniels/Eventix/commit/66c9904f0e14a49bf1590767e4d1fcc3273a5e37"
+        },
+        "date": 1776927696037,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ics/parse_directory/generated_calendar_dir",
+            "value": 37813359.711111106,
             "unit": "ns"
           }
         ]
