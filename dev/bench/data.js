5cf89ed881c5ad7e5a851bceb961d9a1fe0cf7d2
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 11:49:20 +0000

    add Eventix Parse Benchmark (customSmallerIsBetter) benchmark result for 65f0603520518de8cc8491d64d81ed75c4561d79

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777636084698,
+  "lastUpdate": 1777636160504,
   "repoUrl": "https://github.com/hrniels/Eventix",
   "entries": {
     "Eventix List Benchmark": [
@@ -6393,6 +6393,31 @@ window.BENCHMARK_DATA = {
           {
             "name": "ics/parse_directory/generated_calendar_dir",
             "value": 51597884.333333336,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "hrniels",
+            "username": "hrniels"
+          },
+          "committer": {
+            "name": "hrniels",
+            "username": "hrniels"
+          },
+          "id": "65f0603520518de8cc8491d64d81ed75c4561d79",
+          "message": "Improved password source",
+          "timestamp": "2026-05-01T08:52:47Z",
+          "url": "https://github.com/hrniels/Eventix/pull/39/commits/65f0603520518de8cc8491d64d81ed75c4561d79"
+        },
+        "date": 1777636160079,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ics/parse_directory/generated_calendar_dir",
+            "value": 50137562.18333333,
             "unit": "ns"
           }
         ]
