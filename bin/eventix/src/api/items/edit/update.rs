faaa24f64e23074b1e84351e10731aaf95546c55
Nils Asmussen <nils@script-solution.de>
2026-06-21 16:32:39 +0200

    eventix: added translation for component-was-modified error.

--- bin/eventix/src/api/items/edit/update.rs
+++ bin/eventix/src/api/items/edit/update.rs
@@ -56,7 +56,7 @@ fn action_update(
 
     let last_modified = util::system_time_stamp(file.last_modified()?);
     if last_modified > form.edit_start {
-        page.add_error("This component has been modified. Please restart the editing.");
+        page.add_error(locale.translate("error.component_was_modified"));
         return Ok((false, None));
     }
 
